83222266443dba3945d19a79852228586537ed52
Lisa Meyer <lisamermey@gmail.com>
2022-04-20 13:20:00 -0400

    Componenet for InterviewerListItem renders data dynamically

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import classNames from 'classnames';
+import "components/InterviewerListItem.scss";
+
+function InterviewerListItem(props){
+  const { id, name, avatar, selected, setInterviewer } = props;
+
+  const interviewerClass = classNames( 
+    'interviewers__item',
+    {'interviewers__item--selected': selected},
+  );
+
+  return(
+    <li onClick={() => setInterviewer(id)} className={interviewerClass}>
+      <img 
+        className="interviewers__item-image"
+        src={avatar}
+        alt={name}
+      />
+      {selected && name}
+    </li>
+  );
+};
+export default InterviewerListItem;
