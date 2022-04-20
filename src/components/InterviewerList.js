23efa84d3269e1579ffe530804d9f183ae59af46
Lisa Meyer <lisamermey@gmail.com>
2022-04-20 14:16:29 -0400

    Import InterviewerList.js

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,11 +1,26 @@
 import React, { useState } from 'react';
+import InterviewerListItem from './InterviewerListItem';
 import 'components/InterviewerList.scss';
 
 export default function InteviewerList(props){
+  const { interviewers, interviewer, setInterviewer } = props;
+
+  const renderInterviewerItem = interviewers.map(item => 
+    <InterviewerListItem 
+      key={item.id}
+      name={item.name}
+      avatar={item.avatar}
+      selected={item.id === interviewer}
+      setInterviewer={() => setInterviewer(item.id)}
+    />
+  )
+
   return(
     <section className='interviewers'>
       <h4 className='interviewers__header text--light'>Interviewer</h4>
-      <ul className='interviewers__list'></ul>
+      <ul className='interviewers__list'>
+        {renderInterviewerItem}
+      </ul>
     </section>
   );
 };
