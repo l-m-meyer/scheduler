0c713a90576d67bb0bd9d7b9e4487e84d875bb03
Lisa Meyer <lisamermey@gmail.com>
2022-04-20 20:36:48 -0400

    Remove unused function

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -8,11 +8,6 @@ import "components/Appointment/styles.scss";
 export default function Appointment(props){
   const { time, interview } = props;
 
-  const showAppointmentTime = (time) => {
-    if (!time) return 'No appointments'
-    return `Appointment at ${time}`
-  }
-
   return(
     <article className="appointment">
       <Header time={time} />
