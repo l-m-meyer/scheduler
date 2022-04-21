0e12b6e3d939891b22e0ba9a004d242d0134d9c3
Lisa Meyer <lisamermey@gmail.com>
2022-04-20 22:25:50 -0400

    Relocate appointments object to Application.js

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import Header from "components/Appointment/Header";
 import Show from "components/Appointment/Show";
 import Empty from "components/Appointment/Empty";
-
 import "components/Appointment/styles.scss";
 
 export default function Appointment(props){
