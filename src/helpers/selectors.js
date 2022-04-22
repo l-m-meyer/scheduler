64b28771e275bea2e9b6086d064227bacf7b2ad4
Lisa Meyer <lisamermey@gmail.com>
2022-04-22 16:11:16 -0400

    Add selector helper, passes tests

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,23 +1,10 @@
 
 
 
-const getAppointmentsForDay = (state, day) => {
-
-  // 1. find object in state.days that matches provided day
-  const days = Object.values(state.days).filter(day => day.name === day? day.id : null);
-
-  let dayID;
-  for(day of days) {
-    if (day.name === day) {
-      return dayID = day.id
-    }
-  }
-  // 2. iterate through day comparing its id with state.appointments id and return that value
-  let appointmentID;
-  const appointment = Object.values(state.appointments).filter(appointment => dayID === appointment.id ? appointmentID = appointment.id : null);
-
-  // 3. validation
-  // if there are no appointments on day, days data will return empty array
+export default function getAppointmentsForDay (state, day) {
+  const dayAppointments = (state.days).find(item => item.name === day);
   
+  if (!dayAppointments) return [];
 
+  return (dayAppointments.appointments).map(id => state.appointments[id]);
 }
