ba9664a0b4ba6145d24eb32e15aceacfcd3aceb9
Lisa Meyer <lisamermey@gmail.com>
2022-04-22 20:38:39 -0400

    Import useVisualMode hook, add constants

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,10 +2,14 @@ import React, { useState } from 'react';
 import Header from "components/Appointment/Header";
 import Show from "components/Appointment/Show";
 import Empty from "components/Appointment/Empty";
+import useVisualMode from 'hooks/useVisualMode';
 import "components/Appointment/styles.scss";
 
 export default function Appointment(props){
   const { time, interview } = props;
+
+  const EMPTY = 'EMPTY';
+  const SHOW = 'SHOW';
 
   return(
     <article className="appointment">
