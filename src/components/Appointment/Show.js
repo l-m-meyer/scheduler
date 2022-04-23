9b8a4350846b90ff00a0f858bf836f00ded54d2a
Lisa Meyer <lisamermey@gmail.com>
2022-04-22 20:17:38 -0400

    Refactor Show component to render appointments

--- src/components/Appointment/Show.js
+++ src/components/Appointment/Show.js
@@ -6,10 +6,10 @@ export default function Show(props){
   return(
     <main className="appointment__card appointment__card--show">
       <section className="appointment__card-left">
-        <h2 className="text--regular">{student || 'Lydia Miller Jones'}</h2>
+        <h2 className="text--regular">{student}</h2>
         <section className="interviewer">
           <h4 className="text--light">Interviewer</h4>
-          <h3 className="text--regular">{interviewer || 'Sylvia Palmer'}</h3>
+          <h3 className="text--regular">{interviewer}</h3>
         </section>
       </section>
       <section className="appointment__card-right">
