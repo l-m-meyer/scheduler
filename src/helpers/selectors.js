d972dd0d12ef91decb2eab8911d4688ac28ee2af
Lisa Meyer <lisamermey@gmail.com>
2022-04-23 16:16:22 -0400

    Working solution for getInterviewersForDay

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -18,6 +18,26 @@ export const getInterview = (state, interview)  => {
 }
 
 
-export const getInterviewersForDay = () => {
+export const getInterviewersForDay = (state, selectedDayName) => {
+  // console.log('state:',state);
+  // console.log('selectedDayName', selectedDayName);
+  const selectedDay = state.days.find(day => day.name === selectedDayName);
+  // console.log('selectedDay', selectedDay);
+  if (!selectedDay) return [];
 
+  const interviewerIds = selectedDay.interviewers;
+  const interviewers = state.interviewers;
+  const interviewersArr = Object.values(interviewers);
+  // console.log('interviewers', interviewers);
+  console.log('interviewersArr: ', interviewersArr);
+  console.log('interviewerIds:',interviewerIds)
+
+  const selectedDayInterviewers = interviewerIds.map((id) =>  {
+    interviewersArr.find((interviewer) => interviewer.id === id)
+  });
+  console.log('selectedDayInterviewers: ', selectedDayInterviewers);
+
+  return selectedDayInterviewers;
+
+  
 }
