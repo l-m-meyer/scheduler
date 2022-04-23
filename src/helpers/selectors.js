297e3963620ae9c3110b774b369434dc2ebbf755
Lisa Meyer <lisamermey@gmail.com>
2022-04-22 20:21:34 -0400

    Add helper functions for selectors

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,10 +1,18 @@
 
-
-
-export default function getAppointmentsForDay (state, day) {
-  const dayAppointments = (state.days).find(item => item.name === day);
+export const getAppointmentsForDay = (state, day) => {
+  const dayAppointments = state.days.find(item => item.name === day);
   
   if (!dayAppointments) return [];
 
-  return (dayAppointments.appointments).map(id => state.appointments[id]);
+  return dayAppointments.appointments.map(id => state.appointments[id]);
+}
+
+
+export const getInterview = (state, interview)  => {
+  if (!interview) return null;
+
+  return {
+    ...interview, 
+    interviewer: state.interviewers[interview.interviewer]
+  };
 }
