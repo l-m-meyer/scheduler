a90f9ee27e5aa7b9c48f7a63b6dd5b7b9bca92b4
Lisa Meyer <lisamermey@gmail.com>
2022-04-25 18:19:55 -0400

    Remove interview argument from cancelInterview()

--- src/components/Application.js
+++ src/components/Application.js
@@ -14,19 +14,19 @@ export default function Application(props) {
     interviewers: {}
   });
 
-  function cancelInterview(id, interview) {
+  function cancelInterview(id) {
 
     const appointment = {
       ...state.appointments[id],
       interview: null
     }
-    
+
     const appointments = {
       ...state.appointments,
       [id]: appointment
     };
 
-    return axios.delete(`/api/appointments/${id}`, {interview})
+    return axios.delete(`/api/appointments/${id}`)
       .then(() => {
         setState({...state, appointment})
       })
