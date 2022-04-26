f7140094f0d1ae8b665b7380ef96d2f702bd9f19
Lisa Meyer <lisamermey@gmail.com>
2022-04-26 00:55:03 -0400

    Remove unused varibles

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -3,7 +3,7 @@ import classNames from 'classnames';
 import "components/InterviewerListItem.scss";
 
 function InterviewerListItem(props){
-  const { id, name, avatar, selected, setInterviewer } = props;
+  const { name, avatar, selected, setInterviewer } = props;
 
   const interviewerClass = classNames( 
     'interviewers__item',
