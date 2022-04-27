e239adf830add0e76a9c5cb420c8d88f4e200056
Lisa Meyer <lisamermey@gmail.com>
2022-04-27 18:24:35 -0400

    Successful test to cancel an interview

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -45,6 +45,20 @@ describe("Appointments", () => {
   });
 
   it("should cancel an interview", () => {
+    cy.get("[alt=Delete]")
+      .first()
+      .click({ force: true });
+
+    cy.contains("Confirm")
+      .click();
+
+    cy.contains("DELETING")
+      .should("exist");
     
+    cy.contains("DELETING")
+      .should("not.exist");
+
+    cy.contains(".appointment__card--show", "Archie Cohen")
+      .should("not.exist");
   });
 })
