1e4ee7788fd0a167eb199a7910b6599a2fe49a72
Lisa Meyer <lisamermey@gmail.com>
2022-04-27 17:59:12 -0400

    Add beforeEach, test to edit an appointment

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -1,11 +1,14 @@
 describe("Appointments", () => {
-  it("should book an interview", () => {
+
+  beforeEach(() => {
     cy.request("GET", "/api/debug/reset");
-
+  
     cy.visit("/");
-
+  
     cy.contains("Monday");
+  })
 
+  it("should book an interview", () => {
     cy.get("[alt=Add]")
       .first()
       .click();
@@ -21,5 +24,23 @@ describe("Appointments", () => {
 
     cy.contains(".appointment__card--show", "Lydia Miller-Jones");
     cy.contains(".appointment__card--show", "Sylvia Palmer");
-  })
+  });
+
+  it("should edit an appointment", () => {
+    cy.get("[alt=Edit]")
+      .first()
+      .click({ force: true });
+
+    cy.get("[data-testid=student-name-input")
+      .clear()
+      .type("Lydia Miller-Jones");
+
+    cy.get("[alt='Tori Malcolm']")
+      .click();
+    
+    cy.contains("Save").click();
+
+    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+    cy.contains(".appointment__card--show", "Tori Malcolm");
+  });
 })
